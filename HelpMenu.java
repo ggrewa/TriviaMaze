e2cddfdf9a1c5d33338edf6919c9b8b3ae842b01
aedincer22 <93241445+aedincer22@users.noreply.github.com>
2021-11-09 15:04:50 -0800

    Update HelpMenu.java

--- HelpMenu.java
+++ HelpMenu.java
@@ -1,3 +1,22 @@
+package model;
+
 public class HelpMenu {
+    private String instructions;
+    private String hints;
     
+    public HelpMenu() {
+        this.instructions = instructions;
+        this.hints = hints;
+    }
+    
+    public String getInstructions() {
+        return instructions;
+    }
+    public String getHints() {
+        return hints;
+    }
+    
+    public String toString() {
+      return "Instructions: " + intructions + /n + "Hints = " + hints;   
+    }
 }
