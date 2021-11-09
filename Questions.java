011b0450c483d0262ceab0200fd14c264890e7d7
aedincer22 <93241445+aedincer22@users.noreply.github.com>
2021-11-09 14:51:56 -0800

    Update Questions.java

---
+++ Questions.java
@@ -0,0 +1,41 @@
+package model;
+
+public class Questions {
+  
+  // string of a question
+  private String question;
+  // string of an answer for the question
+  private String answer;
+  
+  //an array list holds answers of all questions 
+  private ArrayList<String> answers; 
+  
+  public Questions () {
+    this.question = question;
+    this.answer = answer;
+    this.answers = answers;
+  }
+
+  public String getQuestion() {
+    return question;
+  }
+  public String getAnswer() {
+    return answer;
+  }
+
+  public ArrayList<String> getAnswers() {
+    return answers;
+  }
+
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    
+    sb.append("Question: " + question + "/n");
+    sb.append("Answer: " + answer + "/n");
+    sb.append("All answers: " + ArrayList.toString(answers));
+    
+    return sb.toString();
+
+  }
+
+}
