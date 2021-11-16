62757915a4701c631a298b78df5be17ef60e4aa5
aedincer22 <93241445+aedincer22@users.noreply.github.com>
2021-11-16 15:41:34 -0800

    Update GameOperations.java

--- GameOperations.java
+++ GameOperations.java
@@ -9,7 +9,19 @@ public class GameOperations implements Serializable {
     
   }
   public static void PlayGame(Maze maze) {
+    Interface.printHomeScreen();
+    String homeSelection = Interface.getUserSelectionAtHome();
     
+    if (homeSelection.equals("1")) {
+      Maze maze = new Maze();
+      newGame();
+    } else if (homeSelection.equals("2")) {
+      loadGame();
+    } else if (homeSelection.equals("3")) {
+      Interface.printHelpMenu();
+    } else {
+      System.out.println("Invalid entry!");
+    }
   }  
   public static void exitGame(Maze maze) {
     
