f9f437cc7002c89d3cd0f4a6fd898877f174fbee
aedincer22 <93241445+aedincer22@users.noreply.github.com>
2021-11-16 14:23:04 -0800

    Update Interface.java

--- Interface.java
+++ Interface.java
@@ -14,6 +14,14 @@ public class Interface {
     System.out.println("3-HELP MENU");
   }
   
+  public static void printInGameMenu() {
+    System.out.println("SELECT:" + "\n");
+    System.out.println("1-New Game");
+    System.out.println("2-Load Game");
+    System.out.println("3-HELP MENU");
+    System.out.println("4-Exit");
+  }
+  
   public static void printHelpMenu() {
     System.out.println("++ HELP MENU ++");
     System.out.println("");
