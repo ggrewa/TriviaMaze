032dd4b201e972a5e62cb79416ed77c0513b6723
aedincer22 <93241445+aedincer22@users.noreply.github.com>
2021-11-16 14:20:45 -0800

    Update Interface.java

--- Interface.java
+++ Interface.java
@@ -1,16 +1,23 @@
 public class Interface {
+  priavte static Scanner scanner = new Scanner(System.in);
   
   public static void printHomeScreen() {
-    System.out.println("++ Welcome to TRIVIA MAZE ++");
+    System.out.println("++ Welcome to TRIVIA MAZE ++" + "\n");
     //This part will show instructions,etc.
-    //System.out.println(" ");
-    //System.out.println();
+    System.out.println("InsTructions:");
+    System.out.println();
+    System.out.println("'L' shows locked doors");
+    System.out.println("'X' shows the room you are in" + "\n");
+    System.out.println("SELECT:" + "\n");
+    System.out.println("1-New Game");
+    System.out.println("2-Load Game");
+    System.out.println("3-HELP MENU");
   }
   
   public static void printHelpMenu() {
     System.out.println("++ HELP MENU ++");
-    System.out.println(" New Game - PRESS "1");
-    System.out.println(" Exit - PRESS "2");
+    System.out.println("");
+    System.out.println("");
   }
   
   
