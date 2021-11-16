905aba7673fe205cbc785e4f19a58f6a232a422a
aedincer22 <93241445+aedincer22@users.noreply.github.com>
2021-11-16 11:53:22 -0800

    Update Interface.java

--- Interface.java
+++ Interface.java
@@ -1,7 +1,14 @@
 public class Interface {
   
-  public static void printHelpMenu() {
+  public static void printHomeScreen() {
     System.out.println("++ TRIVIA MAZE ++");
+    //System.out.println();
+    //System.out.println();
+
+  }
+  
+  public static void printHelpMenu() {
+    System.out.println("++ HELP MENU ++");
     //System.out.println();
     //System.out.println();
 
