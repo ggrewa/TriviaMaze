a5f09f71a8be7ecdb396eec717ffc9fcdd770150
aedincer22 <93241445+aedincer22@users.noreply.github.com>
2021-11-16 14:25:03 -0800

    Update Interface.java

--- Interface.java
+++ Interface.java
@@ -4,14 +4,11 @@ public class Interface {
   public static void printHomeScreen() {
     System.out.println("++ Welcome to TRIVIA MAZE ++" + "\n");
     //This part will show instructions,etc.
-    System.out.println("InsTructions:");
-    System.out.println();
-    System.out.println("'L' shows locked doors");
-    System.out.println("'X' shows the room you are in" + "\n");
     System.out.println("SELECT:" + "\n");
     System.out.println("1-New Game");
     System.out.println("2-Load Game");
     System.out.println("3-HELP MENU");
+    System.out.println();
   }
   
   public static void printInGameMenu() {
@@ -20,13 +17,15 @@ public class Interface {
     System.out.println("2-Load Game");
     System.out.println("3-HELP MENU");
     System.out.println("4-Exit");
+    System.out.println();
   }
   
   public static void printHelpMenu() {
     System.out.println("++ HELP MENU ++");
-    System.out.println("");
-    System.out.println("");
+    System.out.println("InsTructions:");
+    System.out.println();
+    System.out.println("'L' shows locked doors");
+    System.out.println("'X' shows the room you are in" + "\n");
+    System.out.println();
   }
-  
-  
 }
