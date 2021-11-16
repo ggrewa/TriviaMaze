65b583448fda1243c138df75981b7da30f950d0c
aedincer22 <93241445+aedincer22@users.noreply.github.com>
2021-11-16 12:07:09 -0800

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,10 +1,17 @@
 package Interface;
 
-import Question.*;
+import Questions.*;
 import Maze.Maze;
 
 import java.io.Serializable;
+import java.util.*;
 
 public class Game implements Serializable {
+  private static numMoves;
+  
+  public static void newGame(Maze maze) {
+    
+  }
   
 }
+
