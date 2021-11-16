b16487ad3f4b23a579283b231a28efdafb68ae56
aedincer22 <93241445+aedincer22@users.noreply.github.com>
2021-11-16 12:19:50 -0800

    Update Maze.java

--- Maze.java
+++ Maze.java
@@ -1,3 +1,22 @@
+import java.io.Serializable;
+import java.util.*;
+
 public class Maze implements Serializable {
+  private static final long serialVersionUID = 1L;
+  private ArrayList[][] rooms;
+  private int rows;
+  private int cols;
+  private ArrayList[][] maze;
   
+  public Maze() {
+    this.rows = rows;
+    this.cols = cols;
+  }
+  public ArrayList rooms (int row, int col) {
+    //
+  }
+  public ArrayList maze (int row, int col) {
+  }
+  public void movement() {
+  } 
 }
