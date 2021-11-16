4f9b3b6dcb7d27aa822d761306595eeecf6bf4a2
aedincer22 <93241445+aedincer22@users.noreply.github.com>
2021-11-16 14:51:28 -0800

    Update Interface.java

--- Interface.java
+++ Interface.java
@@ -1,5 +1,4 @@
 import java.util.*;
-import Maze.Maze;
 
 public class Interface {
   priavte static Scanner scanner = new Scanner(System.in);
@@ -13,6 +12,16 @@ public class Interface {
     System.out.println("3-HELP MENU");
     System.out.println();
   }
+  //make sure user makes a valid entry to select an option from the homescreen menu
+  public static String getUserSelectionAtHome() {
+    String homeSelection = scanner.next();
+    String[] homeOptions = new String[]{"1", "2", "3"};
+    do {
+         System.out.println("Entry is not Valid!");
+         homeSelection = scanner.next();
+    } while(!homeOptions.contains(homeSelection));
+    return homeSelection;
+  }
   
   public static void printInGameMenu() {
     System.out.println("SELECT:" + "\n");
@@ -21,6 +30,17 @@ public class Interface {
     System.out.println("3-HELP MENU");
     System.out.println("4-Exit");
     System.out.println();
+  }
+  
+  //make sure user makes a valid entry to select an option from the in game menu
+  public static String getUserSelectionInGame() {
+    String menuSelection = scanner.next();
+    String[] menuOptions = new String[]{"1", "2", "3", "4"};
+    do {
+         System.out.println("Entry is not Valid!");
+         menuSelection = scanner.next();
+    } while(!menuOptions.contains(menuSelection));
+    return menuSelection;
   }
   
   public static void printHelpMenu() {
