0649b2c8458e7da6ec1bda59adbc487fa7821218
aedincer22 <93241445+aedincer22@users.noreply.github.com>
2021-11-22 22:49:52 -0800

    Update Maze.java

--- Maze.java
+++ Maze.java
@@ -3,40 +3,54 @@ import java.util.*;
 
 public class Maze implements Serializable {
     private static final long serialVersionUID = 1L;
-    private ArrayList[][] rooms;
+    private Room[][] rooms;
     private int rows;
     //default number of rows
-    private int rowsIndex = 4;
+    private int rowsIndex;
     private int cols;
     //default number of cols
-    private int colsIndex = 4;
+    private int colsIndex;
     private ArrayList[][] maze;
-
+    
     public Maze() {
-        this.rowsIndex = rowsIndex;
-        this.colsIndex = colsIndex;
-    }
-    public Maze(int rows, int cols) {
         if(rows < 0 || cols < 0 || rows != cols) {
             throw new IllegalArgumentException("The maze is not in the specified dimension");
         }
+        maze = new Room[4][4];
         rowsIndex = 0;
         colsIndex = 0;
+        buildRooms();
+        buildMaze();
+    }
+
+    public void buildRooms (int rows, int cols) {
+        Door North = new Door();
+        Door South = new Door();
+        Door East = new Door();
+        Door West = new Door();
+        for (int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                boolean north = false;
+                boolean south = false;
+                boolean east = false;
+                boolean west = false;
+                
+                if(cols > 0) {
+                    North = maze[rows][cols - 1].getDoor(1);
+                }
+            }
+        }
+        return rooms;
+
+    }
+    public ArrayList maze (int rows, int cols) {
         
-    }
-
-    public ArrayList rooms (int row, int col) {
-
-    }
-    public ArrayList maze (int row, int col) {
-
     }
 
     public void movement(char direction) {
 
 
     }
-
     public void checkLocation(int row, int col) {
         if(row < 0 || col < 0 || row >= rooms.length || col >= rooms.length) {
             throw new IllegalArgumentException("You are out of bounds");
