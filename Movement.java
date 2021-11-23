cab170a62c5772f28d00d943f8b36d7efcd9d585
aedincer22 <93241445+aedincer22@users.noreply.github.com>
2021-11-22 23:01:22 -0800

    Create Movement.java

---
+++ Movement.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import maze.Maze;
+
+public class Movement {
+    private int rowIndex;
+    private int colIndex;
+  
+    private int DEFAULT_ROW;
+    private int DEFAULT_COL;
+
+  
+}
