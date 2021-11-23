5a7da04c52bea001ed94db068d9035c1b7046a55
aedincer22 <93241445+aedincer22@users.noreply.github.com>
2021-11-22 23:25:08 -0800

    Update Maze.java

--- Maze.java
+++ Maze.java
@@ -43,7 +43,7 @@ public class Maze implements Serializable {
         return rooms;
 
     }
-    public ArrayList maze (int rows, int cols) {
+    public void buildMaze (int rows, int cols) {
         
     }
     
