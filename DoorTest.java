66899f5a04aa19ddf785da8fa81d53731d8fce59
aedincer22 <93241445+aedincer22@users.noreply.github.com>
2021-12-02 13:45:38 -0800

    Update DoorTest.java

--- DoorTest.java
+++ DoorTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class DoorTest extends Door {
+class DoorTest {
 
     @Test
     public void testGetQuestion() {
